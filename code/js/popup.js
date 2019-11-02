ac17b852aeb44f399b60e668529c0fd20110aaa4
ava-toshi <ava-toshi@test.com>
2019-11-02 20:51:42 +0900

    公開対応

--- code/js/popup.js
+++ code/js/popup.js
@@ -1,27 +1,27 @@
 $("#inputDate").on("input", () => {
+  CopyDateToClipboard();
+});
+
+$(() => {
+  $("#dateFormat").change(() => {
+    CopyDateToClipboard();
+  });
+})
+
+// Format Date and Copy it to Clipboard
+function CopyDateToClipboard() {
   // Get date and format
-  var strDate = $("#inputDate").val();
-  var format = $("#dateFormat").val();
+  const strDate = $("#inputDate").val();
+  const format = $("#dateFormat").val();
 
   // Format date and display
   $("#displayDate").val(FormatDate(strDate, format));
-
-  // Copy to clipboard on input date changed
-  CopyDateToClipboard();
-  });
-
-$("#btn").on("click", () => 
-{
-  CopyDateToClipboard();
-})
-
-function CopyDateToClipboard(){
-  if(navigator.clipboard){
+  if (navigator.clipboard) {
     navigator.clipboard.writeText($("#displayDate").val());
   }
 }
 
-function FormatDate(date, format){
+function FormatDate(date, format) {
   moment.locale("ja");
   var momDate = moment(date);
   return momDate.format(format);
